8b2b567b53655e1d54ce2d2a2b44b11efb590ec9
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2022-02-02 04:03:42 +0100

    Update phoneloc.py

--- phoneloc.py
+++ phoneloc.py
@@ -5,12 +5,22 @@ from phonenumbers import carrier
 from phonenumbers import timezone
 import colorama
 from colorama import Fore
-print (Fore.CYAN+"#######################################################################################################")
+print(Fore.RED+f'''
+ ******************************************
+  ********************_______*********|
+  |***********************************************************************************"   |*****************************************
+  |***********"************************************************************************"
+  | ******      {Fore.GREEN+"PHONE LOC"}***************************************************
+ |***********************************************************************************"   |********************************************
+  ''')
+print (Fore.LIGHTBLUE_EX+"###########################################################")
 print(Fore.GREEN+"[//] Created By Paul Fruitful")
 print(Fore.GREEN+"""[//] Contact Me On Whatsapp At +2348114483062 \n[//] Mail Me at fruitful2007@outlook.com""")
-print (Fore.CYAN+"#######################################################################################################")
+print (Fore.LIGHTBLUE_EX+"###########################################################")
 print(Fore.LIGHTYELLOW_EX+"""
-This Tool Is Used For Tracking Phones Just Using Their Numbers 
+This Tool Is Used For Tracking Phones Just Using Their Numbers and it also gives you information about the phone number
+""")
+print(Fore.RED+"""DISCLAIMER: This Tool Is For Educational Purposes Only!!!! 
 """)
 update=input (Fore.GREEN+"Do You Want To Update [y/n]")
 if update=='y':
@@ -18,7 +28,7 @@ if update=='y':
             gitclone https://github.com/paulfruitful/phoneloc""")
 else:
  print("Always Update")
-input=input(Fore.LIGHTBLUE_EX+"Please Input Number You Want To Track:")
+input=input(Fore.CYAN+"Please Input Number You Want To Track:")
 
 phonenumbertrack=phonenumbers.parse(input)
 track=geocoder.description_for_number(phonenumbertrack,'en')
