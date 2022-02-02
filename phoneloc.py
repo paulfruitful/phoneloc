419b8b86cc49c1d64a808f449775b6a17c625e0a
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2022-02-02 03:36:55 +0100

    Update phoneloc.py

--- phoneloc.py
+++ phoneloc.py
@@ -1,23 +1,29 @@
+import os
 import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import timezone
 import colorama
 from colorama import Fore
+print (Fore.CYAN+"#######################################################################################################")
 print(Fore.GREEN+"[//] Created By Paul Fruitful")
-print(Fore.CYAN+"""
-[//] Contact Me On Whatsapp At +2348114483062 Or Mail Me at fruitful2007@outlook.com
-""")
-print(Fore.MAGENTA+"Welcome To Phoneloc")
+print(Fore.GREEN+"""[//] Contact Me On Whatsapp At +2348114483062 \n[//] Mail Me at fruitful2007@outlook.com""")
+print (Fore.CYAN+"#######################################################################################################")
 print(Fore.LIGHTYELLOW_EX+"""
 This Tool Is Used For Tracking Phones Just Using Their Numbers 
 """)
+update=input (Fore.GREEN+"Do You Want To Update [y/n]")
+if update=='y':
+   os.system("""cd 
+            gitclone https://github.com/paulfruitful/phoneloc""")
+else:
+ print("Always Update")
 input=input(Fore.LIGHTBLUE_EX+"Please Input Number You Want To Track:")
 
 phonenumbertrack=phonenumbers.parse(input)
 track=geocoder.description_for_number(phonenumbertrack,'en')
 time=timezone.time_zones_for_number(phonenumbertrack)
 carriername=carrier.name_for_number(phonenumbertrack,'en')
-print(f'''Location:{track} Provider
+print(Fore.GREEN+f'''Location:{track} Provider
 Name:{carriername} 
 Timezone:{time}''')
